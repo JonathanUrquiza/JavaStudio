a0f48aac7b44436746b06b79b72e51bc33f1f953
Jonathan Urquiza <jurquiza86@hotmail.com>
2023-07-31 16:25:59 -0300

    control do while

---
+++ Dowhile.java
@@ -0,0 +1,10 @@
+public class Dowhile {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 1;
+        do {
+            System.out.print(i + ", ");
+            i = i + 1;
+        } while (i <= 10);
+    }
+}
