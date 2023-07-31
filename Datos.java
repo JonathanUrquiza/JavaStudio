29893ae8f68102ec6fd5af5105d2e2a7a683801d
Jonathan Urquiza <jurquiza86@hotmail.com>
2023-07-31 16:24:55 -0300

    datos desde teclado

---
+++ Datos.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Datos {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        String nombre = "";
+        int numeroUno = 0, numDos = 0, resultado = 0;
+
+        System.out.println("cual estu nombre?");
+        nombre = in.nextLine();
+
+        System.out.println("Dame el primer valor para la suma");
+        numeroUno = in.nextInt();
+        System.out.println("Dame el segundo valor para la suma");
+        numDos = in.nextInt();
+        resultado = numeroUno + numDos;
+        System.out.println("hola " + nombre + " El resultado de la suma es " + resultado);
+    }
+}
