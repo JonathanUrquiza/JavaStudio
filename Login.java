559bb27d4521eb1aaee62893abd1a104bfed70cb
Jonathan Urquiza <jurquiza86@hotmail.com>
2023-07-31 22:15:11 -0300

    login en java

---
+++ Login.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Login {
+
+    public static void main(String args[]) {
+
+        String usuario = "", password = "";
+        Scanner entrada = new Scanner(System.in);
+
+        // Solicitud de usuario
+        System.out.println("Ingresa el nombre de usuario: ");
+        usuario = entrada.nextLine();
+
+        // Solicitud de contraseña
+        System.out.println("Ingresa tu contraseña: ");
+        password = entrada.nextLine();
+
+        if (usuario.equals("Jonathan") && password.equals("Jonas")) {
+            System.out.println("Inicio de secion correcto!!");
+        } else {
+            System.out.println("nombre de usuario o contraseña incorrectos");
+        }
+
+    }
+}
