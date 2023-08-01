8edd0e6bc9ff1e2f7b8235c770025e8c2d1b52b2
Jonathan Urquiza <jurquiza86@hotmail.com>
2023-08-01 00:20:51 -0300

    vectoes dinamicos

---
+++ DinamicArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DinamicArray {
+    public static void main(String args[]) {
+
+        int longitud = 0;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Cuantos numeros deseas ingresar?");
+        longitud = entrada.nextInt();
+
+        int numeros[] = new int[longitud];
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Por fabor dame el valor #" + (i + 1));
+            numeros[i] = entrada.nextInt();
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("[" + numeros[i] + "]");
+        }
+    }
+}
