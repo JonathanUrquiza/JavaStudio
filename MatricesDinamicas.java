655cf763a411fbb242a8bc06519e82ba1893d84d
Jonathan Urquiza <jurquiza86@hotmail.com>
2023-08-01 06:21:16 -0300

    matrices dinamicas 2

--- MatricesDinamicas.java
+++ MatricesDinamicas.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class MatricesDinamicas {
     public static void main(String[] args) {
 
         int filas = 3, columnas = 3, contador = 1;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Cuantas filas deseas?");
+        filas = entrada.nextInt();
+
+        System.out.println("cuantas columnas deseas?");
+        columnas = entrada.nextInt();
         int numeros[][] = new int[filas][columnas];
 
         for (int j = 0; j < filas; j++) {
