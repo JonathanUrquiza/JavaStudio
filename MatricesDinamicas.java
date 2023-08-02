34183fae043eea58eb059078baa63bf27f84f90c
Jonathan Urquiza <jurquiza86@hotmail.com>
2023-08-02 01:00:52 -0300

    correción

--- MatricesDinamicas.java
+++ MatricesDinamicas.java
@@ -4,13 +4,14 @@ public class MatricesDinamicas {
     public static void main(String[] args) {
 
         int filas = 3, columnas = 3, contador = 1;
-        Scanner entrada = new Scanner(System.in);
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Cuantas filas deseas?");
+            filas = entrada.nextInt();
 
-        System.out.println("Cuantas filas deseas?");
-        filas = entrada.nextInt();
+            System.out.println("cuantas columnas deseas?");
+            columnas = entrada.nextInt();
+        }
 
-        System.out.println("cuantas columnas deseas?");
-        columnas = entrada.nextInt();
         int numeros[][] = new int[filas][columnas];
 
         for (int j = 0; j < filas; j++) {
